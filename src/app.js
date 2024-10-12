24ee484fc9fb4454b770d7494e9f1af692398159
sujitdudhe0452 <sujitdudhe.2003@gmail.com>
2024-10-12 21:22:14 +0530

    created or playing with Api

--- src/app.js
+++ src/app.js
@@ -3,15 +3,32 @@ const express = require('express');
 const app = express();
 
 
-app.use("/",(req,res)=>{
-    res.send("welcome to the server !");
-});
- 
+
 app.use("/testing",(req,res)=>{
     res.send("Hello from the server!");
 });
 
+app.get("/user",(req,res)=>
+{
+    res.send({firstname:"sujit",lastname:"dudhe"})
+})
 
+app.post("/user",(req,res)=>
+    {
+        console.log("save to the data database");
+        res.send("data save successfully");
+    })
+
+    
+    app.delete("/user",(req,res)=>
+        {
+            console.log("delete to the data database");
+            res.send("data delete successfully");
+        })
+    
+    
+        
+    
 app.listen(3000,()=>{
     console.log("Servers is successfully listnening port numert 3000");
 });
